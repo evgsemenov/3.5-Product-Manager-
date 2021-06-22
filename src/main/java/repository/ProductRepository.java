73fd749e4b81b8f562e0890d8f088f255606f270
Evgenii Semenov <eo.semenoff@gmail.com>
2021-06-22 18:19:46 +0300

    ProductRepository added

--- src/main/java/repository/ProductRepository.java
+++ src/main/java/repository/ProductRepository.java
@@ -1,4 +1,40 @@
 package repository;
 
+import domain.Product;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+
 public class ProductRepository {
+    private Product[] products = new Product[0];
+
+    public void save(Product product){
+        int resultLength = products.length + 1;
+        Product[] tmp = new Product[resultLength];
+        System.arraycopy(products, 0, tmp, 0, products.length);
+        int lastIndex = tmp.length - 1;
+        tmp[lastIndex] = product;
+        products = tmp;
+    }
+
+    public Product[] findAll(){
+        return products;
+    }
+
+    public removeById(int id){
+        int length = products.length - 1;
+        Product[] tmp = new Product[length];
+        int index = 0;
+        for (Product product : products) {
+            if (product.getId() != id) {
+                tmp[index] = product;
+                index++;
+            }
+        }
+        products = tmp;
+    }
 }
+
